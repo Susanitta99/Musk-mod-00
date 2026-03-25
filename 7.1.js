ad84697dc94de4dddfca0ea6f239882b23fa6cd4
Susanitta99 <suziyuzekchieva99@gmail.com>
2026-03-25 12:44:38 +0100

    Delete 7.1.js
    Delete this file

--- 7.1.js
+++
@@ -1,8 +0,0 @@
-const readLine = require("readline-sync")
-const radio = readLine.question("Introduce el radio: ");
-
-function calcularArea(radio) {
-    const area = 2 * Math.PI * radio ;
-    console.log(`El área del círculo es ${area}`);
-   }
-    calcularArea(radio);
